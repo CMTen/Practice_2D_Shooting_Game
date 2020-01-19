8ddaf2c8b1c5514fc00d8763948b2d3314ee5c80
CMTen <57666274+CMTen@users.noreply.github.com>
2020-01-19 15:40:16 +0800

    更新

--- UnityProject/Assets/Scripts/White.cs
+++ UnityProject/Assets/Scripts/White.cs
@@ -3,7 +3,28 @@
 public class White : MonoBehaviour
 {
     #region 練習區域 - 在此區域內練習
-    
+
+    [Header("子彈")]
+    public GameObject bullet;
+    [Header("子彈發射位置")]
+    public Transform shootpos;
+    [Header("音效")]
+    public AudioSource aud;
+    public AudioClip audc;
+
+    /// <summary>
+    /// 射擊
+    /// </summary>
+    private void Shoot()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            Quaternion rot = new Quaternion(0, 0, 0, 0);
+            Instantiate(bullet, shootpos.position, rot);
+            aud.PlayOneShot(audc);
+        }
+    }
+
     #endregion
 
     #region KID 區域 - 不要偷看 @3@
@@ -20,6 +41,7 @@ public class White : MonoBehaviour
     private void FixedUpdate()
     {
         Move();
+        Shoot();
     }
 
     /// <summary>
